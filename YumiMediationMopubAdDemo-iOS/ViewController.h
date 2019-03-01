16dfe88f35b5825ca09b01d13966de9b22eb17e3
ShunZhi Tang <tangshunzhi@zplay.com>
2019-03-01 11:16:27 +0800

    Initial Commit

---
+++ YumiMediationMopubAdDemo-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YumiMediationMopubAdDemo-iOS
+//
+//  Created by Michael Tang on 2019/3/1.
+//  Copyright © 2019 MichaelTang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YumiMediationMopubAdDemo-iOS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YumiMediationMopubAdDemo-iOS
+//
+//  Created by Michael Tang on 2019/3/1.
+//  Copyright © 2019 MichaelTang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
