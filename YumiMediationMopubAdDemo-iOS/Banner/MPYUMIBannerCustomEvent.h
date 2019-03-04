4ea53549ed3732fea6be1b495a082f73ab45e5cb
ShunZhi Tang <tangshunzhi@zplay.com>
2019-03-04 11:02:46 +0800

    Add MPYUMIBannerCustomEvent

---
+++ YumiMediationMopubAdDemo-iOS/Banner/MPYUMIBannerCustomEvent.h
@@ -0,0 +1,17 @@
+//
+//  MPYUMIBannerCustomEvent.h
+//  YumiMediationMopubAdDemo-iOS
+//
+//  Created by Michael Tang on 2019/3/4.
+//  Copyright © 2019 MichaelTang. All rights reserved.
+//
+
+#import "MPBannerCustomEvent.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPYUMIBannerCustomEvent : MPBannerCustomEvent
+
+@end
+
+NS_ASSUME_NONNULL_END
