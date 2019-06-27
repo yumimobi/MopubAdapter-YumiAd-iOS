5b38434b43e547425ad1efba0c447cde4f1a5948
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-27 15:51:33 +0800

    Modify video custom event

--- YumiMediationMopubAdDemo-iOS/Reward Video/MPYUMIRewardedVideoCustomEvent.m
+++ YumiMediationMopubAdDemo-iOS/Reward Video/MPYUMIRewardedVideoCustomEvent.m
@@ -15,16 +15,10 @@ static NSUInteger refreshMaxCount = 6;
 @interface MPYUMIRewardedVideoCustomEvent() <YumiMediationVideoDelegate>
 
 @property (nonatomic) YumiMediationVideo  *rewardedVideo;
-@property (nonatomic) dispatch_source_t timer;
-@property (nonatomic , assign)int refreshCount;
+
 @end
 
 @implementation MPYUMIRewardedVideoCustomEvent
-
-- (void)dealloc{
-    
-    [self cancelTimer];
-}
 
 -(void)requestRewardedVideoWithCustomEventInfo:(NSDictionary *)info{
     NSString *placementId =  [info objectForKey:@"placementId"];
@@ -34,10 +28,6 @@ static NSUInteger refreshMaxCount = 6;
     self.rewardedVideo = [YumiMediationVideo sharedInstance];
     self.rewardedVideo.delegate = self;
     [self.rewardedVideo loadAdWithPlacementID:placementId channelID:channelId versionID:versionId];
-    
-    // start timer
-    [self startTimer];
-    self.refreshCount = -1;
 }
 
 - (BOOL)hasAdAvailable{
@@ -51,81 +41,41 @@ static NSUInteger refreshMaxCount = 6;
     return YES;
 }
 
-#pragma mark: private method
-- (void)startTimer
-{
-    __weak typeof(self) weakSelf = self;
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
-    
-    dispatch_source_set_timer(self.timer, dispatch_walltime(NULL, 0), refreshTimedDration * NSEC_PER_SEC, 0);
-    dispatch_source_set_event_handler(self.timer, ^{
-        [weakSelf refreshMessage];
-    });
-    dispatch_resume(self.timer);
-}
-- (void)refreshMessage
-{
-    self.refreshCount = self.refreshCount + 1;
-    
-    __weak typeof(self) weakSelf = self;
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        [weakSelf handleRewardedVideoLoadState];
-    });
-}
+#pragma mark: YumiMediationVideoDelegate
 
-- (void)handleRewardedVideoLoadState{
-    
-    if (![self.rewardedVideo isReady]) {
-        if (self.refreshCount == refreshMaxCount) { // 60s time out
-            [self notifyRewardedVideoLoadFail];
-        }
-        return;
-    }
-    
+- (void)yumiMediationVideoDidReceiveAd:(YumiMediationVideo *)video {
     if ([self.delegate respondsToSelector:@selector(rewardedVideoDidLoadAdForCustomEvent:)]) {
         [self.delegate rewardedVideoDidLoadAdForCustomEvent:self];
     }
-    // if isready cancel timer
-    [self cancelTimer];
 }
 
-- (void)notifyRewardedVideoLoadFail{
-    NSError *error = [NSError errorWithDomain:@"" code:501 userInfo:@{@"fail reason" : @"request time out"}];
+- (void)yumiMediationVideo:(YumiMediationVideo *)video didFailToLoadWithError:(NSError *)error {
     if ([self.delegate respondsToSelector:@selector(rewardedVideoDidFailToPlayForCustomEvent:error:)]) {
         [self.delegate rewardedVideoDidFailToPlayForCustomEvent:self error:error];
     }
-    
-    [self cancelTimer];
-    
 }
 
-- (void)cancelTimer{
-    if (self.timer) {
-        dispatch_source_cancel(self.timer);
-        self.timer = nil;
+- (void)yumiMediationVideo:(YumiMediationVideo *)video didFailToShowWithError:(NSError *)error {
+    if ([self.delegate respondsToSelector:@selector(rewardedVideoDidFailToPlayForCustomEvent:error:)]) {
+        [self.delegate rewardedVideoDidFailToPlayForCustomEvent:self
+                                                          error:error];
     }
     
 }
-
-#pragma mark: YumiMediationVideoDelegate
-
-- (void)yumiMediationVideoDidOpen:(YumiMediationVideo *)video{
+    
+- (void)yumiMediationVideoDidOpen:(YumiMediationVideo *)video {
     if ([self.delegate respondsToSelector:@selector(rewardedVideoWillAppearForCustomEvent:)]) {
         [self.delegate rewardedVideoWillAppearForCustomEvent:self];
     }
 }
-
-/// Tells the delegate that the video ad started playing.
-- (void)yumiMediationVideoDidStartPlaying:(YumiMediationVideo *)video{
+    
+- (void)yumiMediationVideoDidStartPlaying:(YumiMediationVideo *)video {
     if ([self.delegate respondsToSelector:@selector(rewardedVideoDidAppearForCustomEvent:)]) {
         [self.delegate rewardedVideoDidAppearForCustomEvent:self];
     }
 }
-
-/// Tells the delegate that the video ad closed.
-- (void)yumiMediationVideoDidClose:(YumiMediationVideo *)video{
+    
+- (void)yumiMediationVideoDidClosed:(YumiMediationVideo *)video isRewarded:(BOOL)isRewarded{
     if ([self.delegate respondsToSelector:@selector(rewardedVideoWillDisappearForCustomEvent:)]) {
         [self.delegate rewardedVideoWillDisappearForCustomEvent:self];
     }
@@ -133,11 +83,16 @@ static NSUInteger refreshMaxCount = 6;
         [self.delegate rewardedVideoDidDisappearForCustomEvent:self];
     }
 }
-
-/// Tells the delegate that the video ad has rewarded the user.
-- (void)yumiMediationVideoDidReward:(YumiMediationVideo *)video{
+- (void)yumiMediationVideoDidReward:(YumiMediationVideo *)video {
     if ([self.delegate respondsToSelector:@selector(rewardedVideoShouldRewardUserForCustomEvent:reward:)]) {
         [self.delegate rewardedVideoShouldRewardUserForCustomEvent:self reward:nil];
     }
 }
+    
+- (void)yumiMediationVideoDidClick:(YumiMediationVideo *)video {
+    if ([self.delegate respondsToSelector:@selector(rewardedVideoDidReceiveTapEventForCustomEvent:)]) {
+        [self.delegate rewardedVideoDidReceiveTapEventForCustomEvent:self];
+    }
+}
+
 @end
